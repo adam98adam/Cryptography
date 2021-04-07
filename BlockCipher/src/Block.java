64a71e1ca866dee9db0bcd5ec74fb5292309cf00
adam98adam <adamzebr98@gmail.com>
2021-04-07 13:26:12 +0200

    ecb and cbc

--- BlockCipher/src/Block.java
+++ BlockCipher/src/Block.java
@@ -1,55 +1,22 @@
 import javax.imageio.ImageIO;
-import javax.imageio.ImageReadParam;
-import javax.imageio.ImageReader;
-import javax.imageio.stream.ImageInputStream;
-import javax.swing.*;
-import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.awt.image.RenderedImage;
-import java.awt.image.WritableRaster;
+import java.util.Random;
 import java.io.*;
-import java.util.Arrays;
-import java.util.Iterator;
 
 public class Block {
-
-
-    public static void ecbEncrypt1d() throws IOException {
-        int[] key = {1,0,1,1,1,1,1,0,1,0,1,1,1,0,0,1};
-        BufferedImage bImage = ImageIO.read(new File("files/plain24bit.bmp"));
-        System.out.println("Type = " + bImage.getType());
+    public static void ecbEncrypt(String path) throws IOException {
+        int[] key;
+        Random rnd = new Random();
+        BufferedImage bImage = ImageIO.read(new File(path));
         int width = bImage.getWidth();
         int height = bImage.getHeight();
-        int[] bmpPixels = bImage.getRGB(0, 0, width, height, null, 0, width);
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ImageIO.write(bImage, "bmp", bos);
-        for(int i=0;i < 9000;i++) {
-            for(int k=0;k < 4*4;k++) {
-                if(key[k] == 1) {
-                    if(bmpPixels[k + i*16] == -1)
-                        bmpPixels[k + i*16] = -16777216;
-                    else
-                        bmpPixels[k + i*16] = -1;
-                }
-            }
-        }
-        bImage.setRGB(0,0,width,height,bmpPixels,0,width);
-        ImageIO.write(bImage,"bmp",new File("files/output1.bmp"));
-        System.out.println("Done");
-
-
-    }
-
-    public static void ecbEncrypt() throws IOException {
-        int[] key = {1,1,0,1,0,0,1,1,0,1,1,1,0,1,1,1};
-        BufferedImage bImage = ImageIO.read(new File("files/plain24bit.bmp"));
-        int width = bImage.getWidth();
-        int height = bImage.getHeight();
-        int blockWidthCount = (width / 4);
-        int blockHeightCount = (height / 4);
         int blockPixelWidth = 4;
         int blockPixelHeight = 4;
+        int blockWidthCount = (width / blockPixelWidth);
+        int blockHeightCount = (height / blockPixelHeight);
+        key= new int[blockPixelHeight * blockPixelWidth];
+        for(int i=0;i < blockPixelWidth * blockPixelHeight;i++)
+            key[i] = rnd.nextInt(2);
         for (int xblock = 0; xblock < blockHeightCount; xblock++) {
             for (int yblock = 0; yblock < blockWidthCount; yblock++) {
                 int count = 0;
@@ -65,45 +32,49 @@ public class Block {
                     }
             }
         }
-        ImageIO.write(bImage,"bmp",new File("files/output2.bmp"));
+        ImageIO.write(bImage,"bmp",new File("files/ecb_crypto.bmp"));
     }
 
-
-
-
-
-        //int[] bmpPixels = bImage.getRGB(0, 0, width, height, null, 0, width);
-        //System.out.println("Size = " + bmpPixels.length);
-        //ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        //ImageIO.write(bImage, "bmp", bos);
-        //for(int i=0;i < 9000;i++) {
-            //for(int k=0;k < 4*4;k++) {
-                //if(key[k] == 1) {
-                   // if(bmpPixels[k + i*16] == -1)
-                     //   bmpPixels[k + i*16] = -16777216;
-                    //else
-                     //   bmpPixels[k + i*16] = -1;
-                //}
-           // }
-
-        //bImage.setRGB(0,0,width,height,bmpPixels,0,width);
-       // ImageIO.write(bImage,"bmp",new File("files/ecboutput.bmp"));
-        //System.out.println("Done ebc");
-        //Color c = new Color(-16777216,false);
-
-
-    public static void cbcEncryption() throws IOException {
-        int[] key = {1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1};
-        BufferedImage bImage = ImageIO.read(new File("files/plain24bit.bmp"));
+    public static void cbcEncryption(String path) throws IOException {
+        int[] vector,key;
+        Random rnd = new Random();
+        BufferedImage bImage = ImageIO.read(new File(path));
         int width = bImage.getWidth();
         int height = bImage.getHeight();
-
+        int blockPixelWidth = 4;
+        int blockPixelHeight = 4;
+        int blockWidthCount = (width / blockPixelWidth);
+        int blockHeightCount = (height / blockPixelHeight);
+        vector = new int[blockPixelWidth * blockPixelHeight];
+        key = new int[blockPixelWidth * blockPixelHeight];
+        for(int i=0;i < blockPixelWidth * blockPixelHeight;i++ ) {
+            vector[i] = rnd.nextInt(2);
+            key[i] = rnd.nextInt(2);
+        }
+        for (int xblock = 0; xblock < blockHeightCount; xblock++) {
+            for (int yblock = 0; yblock < blockWidthCount; yblock++) {
+                int count = 0;
+                for (int x = blockPixelHeight * xblock; x < blockPixelHeight * (xblock + 1); x++) {
+                    for (int y = blockPixelWidth * yblock; y < blockPixelWidth * (yblock + 1); y++) {
+                        if (bImage.getRGB(y, x) == -16777216)
+                            vector[count] = 1 ^ vector[count];
+                        if (key[(count * x * y) % (count + 1)] == 1) {
+                            bImage.setRGB(y, x, -16777216);
+                            vector[count] = 1;
+                        } else {
+                            bImage.setRGB(y, x, -1);
+                            vector[count] = 0;
+                        }
+                        count++;
+                    }
+                }
+            }
+        }
+        ImageIO.write(bImage,"bmp",new File("files/cbc_crypto.bmp"));
     }
 
-
     public static void main(String[] args) throws IOException {
-        //ecbEncrypt1d();
-        ecbEncrypt();
-        //cbcEncryption();
+        ecbEncrypt("files/plain24bit.bmp");
+        cbcEncryption("files/plain24bit.bmp");
     }
 }
